047cde062fc141d11c068124e0973e380f3ce2a9
rettwalker <rettwalker@gmail.com>
2018-10-22 21:20:25 -0400

    adding split

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name = "DBS React DEMO"
+let frameworks = [{
+  name: 'React',
+  description: 'A library'
+},
+{
+  name: 'Angular',
+  description: 'A framework'
+}, {
+  name: 'VUE',
+  description: 'A framework'
+}, {
+  name: 'AngularJS',
+  description: 'A framework'
+}, {
+  name: 'Ember',
+  description: 'A framework'
+}, {
+  name: 'jQuery',
+  description: 'hmmm'
+}]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="d-flex flex-row">
+          <div className="w-25"></div>
+          <div className="w-75"></div>
+        </div>
       </div>
     );
   }
